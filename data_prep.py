d6caa85cc8bd9794e342d0f569eee40c18405933
brennan4 <brennan4@stanford.edu>
2023-02-24 21:52:11 -0800

    processing more data

--- data_prep.py
+++ data_prep.py
@@ -7,7 +7,7 @@ def saveAsJSON(f, d):
         json.dump(d, outfile)
 
 def loadJSON(f):
-    with open(f) as json_file:
+    with open(f, 'r') as json_file:
         data = json.load(json_file)
     return data
 
@@ -21,23 +21,19 @@ def format_caption(text):
 
 def format_data(data):
     full_data = {}
-    for i, post in enumerate(data):
-        # if i == 0:
-        #     print('post type', post["type"])
-        #     print('post sponsored', post["isSponsored"])
-        #     print('len hashtags', len(post["hashtags"]))
-        # if (post["type"] == "Image" or post["type"] == "Sidecar") and not post["isSponsored"] and not post["paidPartnership"] and "#ad" not in post["hashtags"]:
-        if (post["type"] == "Image" or post["type"] == "Sidecar") and not post["isSponsored"] and len(post["hashtags"]) == 0:
+    for post in data:
+        # if (post["type"] == "Image" or post["type"] == "Sidecar") and not post["isSponsored"] and len(post["hashtags"]) == 0:
+        if (post["type"] == "Image" or post["type"] == "Sidecar") and not post["isSponsored"] and "#ad" not in post["hashtags"]:
             if ("paidPartnership" in post and not post["paidPartnership"]) or "paidPartnership" not in post:
-                caption = format_caption(post["caption"])
+                caption = format_caption(post["caption"]) if len(post["caption"]) > 0 else '<NO-CAPTION>'
                 for image in post["images"]:
                     full_data[image] = {"true_caption": caption}
     return full_data
 
 def main():
-    data = loadJSON('celeb_data.json')
+    data = loadJSON('normal_ppl_posts_pt1.json')
     data = format_data(data)
-    saveAsJSON('processed_celeb_data.json', data)
+    saveAsJSON('processed_normal_ppl_data_pt1.json', data)
 
 if __name__ == '__main__':
     main()

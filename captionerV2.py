6347c1699f5988bff9e92db78f17a51d0b2e57ea
Grace Alwan <grace.alwan@gmail.com>
2023-02-24 21:49:01 -0800

    fixed captioner

---
+++ captionerV2.py
@@ -0,0 +1,25 @@
+import json
+from transformers import pipeline
+
+image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+
+def read_data(filename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+        return data
+    
+
+data_obj = read_data('./processed_celeb_data.json')
+
+with open("influencerOutput.json", "w") as outfile:
+    for i, img in enumerate(data_obj):
+        plain_text_caption_obj = image_to_text(img)
+        gen_caption = plain_text_caption_obj[0]['generated_text']
+        # is this how they want the data?
+        true_caption = data_obj[img]['true_caption']
+        if (i % 50 == 0):
+            print(i)
+        entry = {"text": "caption: {plain_caption}\ngen-z social media caption: {gen_z_cap}".format(plain_caption = gen_caption, gen_z_cap = true_caption)}
+        # fine_tuning_data.append(entry)
+        json_object = json.dumps(entry, indent=4)
+        outfile.write(json_object)

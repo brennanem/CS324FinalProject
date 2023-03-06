c45be12f192bd28c9431c6da4ad05705f1e5454b
Christine Manegan <christinemanegan@DN0a22640f.SUNet>
2023-03-06 14:45:38 -0800

    added full_pipeline

--- full_pipeline.py
+++ full_pipeline.py
@@ -7,37 +7,47 @@ from PIL import UnidentifiedImageError
 
 
 
-img = "" #TODO: figure out how to get image from user
+
+img = "grace_isaac.jpg" #TODO: figure out how to get image from user
 
 image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
 
+
 tokenizer = AutoTokenizer.from_pretrained("xzyao/HWNWZ5B9AM9PSXV09BAF24ADGP97LMCN2LFQTZJNRQF318SMFM") 
+model = AutoModelForCausalLM.from_pretrained("xzyao/HWNWZ5B9AM9PSXV09BAF24ADGP97LMCN2LFQTZJNRQF318SMFM")
 
-try:
-	plain_text_caption_obj = image_to_text(img)
-	plain_text_caption = plain_text_caption_obj[0]['generated_text']
-	
+print("downloaded tokenizer + model")
 
-	prompt = ("" + plain_text_caption)
+# try:
+plain_text_caption_obj = image_to_text(img)
+plain_text_caption = plain_text_caption_obj[0]['generated_text']
 
-	try:
-		input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
+print("plaintext caption: ", plain_text_caption)
 
-		gen_tokens = model.generate(
-			input_ids,
-			do_sample=True,
-			temperature=0.9 #can change to increase randomness for gen z
-			max_length = 80, #can toggle length for shorrter gen-z captions
-		)
+with open("prompt.txt") as f: 
+	plain_prompt = "".join(f.readlines())
 
-		gen_text = tokenizer.batch_decode(gen_tokens)[0]
-		print(gen_text)
+prompt = plain_prompt.replace("<ADD-PLAIN-TEXT-CAPTION>", plain_text_caption)
+print("FULL PROMPT: ", prompt)
 
-	except:
-		print("tokenizer error")
+# try:
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
 
-except:
-	print("image error")
+gen_tokens = model.generate(
+	input_ids,
+	do_sample=True,
+	temperature=0.9, #can change to increase randomness for gen z
+	max_length = 80, #can toggle length for shorrter gen-z captions
+)
+
+gen_text = tokenizer.batch_decode(gen_tokens)[0]
+print(gen_text)
+
+# except:
+# 	print("tokenizer error")
+
+# except:
+# 	print("image error")
 
 
 

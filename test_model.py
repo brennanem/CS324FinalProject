1d77f7a8d0c7bc10be0c0ff20927451485af3a51
brennan4 <brennan4@stanford.edu>
2023-03-06 18:18:07 -0800

    run evalution

--- test_model.py
+++ test_model.py
@@ -5,6 +5,8 @@ from PIL import UnidentifiedImageError
 from transformers import pipeline
 from PIL import UnidentifiedImageError
 import csv
+import pandas as pd
+import re
 
 
 
@@ -16,10 +18,6 @@ def load_data(filename):
     return data
 
 
-if __name__ == '__main__':
-    main()
-
-
 def main():
 	tokenizer = AutoTokenizer.from_pretrained("xzyao/HWNWZ5B9AM9PSXV09BAF24ADGP97LMCN2LFQTZJNRQF318SMFM") 
 	model = AutoModelForCausalLM.from_pretrained("xzyao/HWNWZ5B9AM9PSXV09BAF24ADGP97LMCN2LFQTZJNRQF318SMFM")
@@ -27,13 +25,13 @@ def main():
 		plain_prompt = "".join(f.readlines())
 	test_data = "test_data_FINAL_2.jsonl"
 	data = load_data(test_data)
-	final_captions = []
-	for post in data: 
+	print("num test examples", len(data))
+	eval_data = {"plain_text_caption": [], "true_gen_z_caption": [], "generated_caption": []}
+	for i, post in enumerate(data): 
+		print("example", i)
 		string_text = post["text"]
-		end = string_text.find("\n") - 1
-		plain_text_caption = string_text[:end]
-		colon = string_text.find(":")
-		gen_z_caption = string_text[colon + 2:]
+		plain_text_caption = re.search(r"caption: (.*) \n", string_text).groups()[0]
+		gen_z_caption = re.search(r"gen-z social media caption: (.*)", string_text).groups()[0]
 		prompt = plain_prompt.replace("<ADD-PLAIN-TEXT-CAPTION>", plain_text_caption)
 
 		input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(model.device)
@@ -41,24 +39,22 @@ def main():
 		gen_tokens = model.generate(
 			input_ids,
 			do_sample=True,
-			temperature=0.9, #can change to increase randomness for gen z
-			max_length = 80, #can toggle length for shorrter gen-z captions
+			temperature=1.0,
+			max_new_tokens=20,
+			eos_token_id=198, #new line token
+			pad_token_id=198,
 		)
-
 		gen_text = tokenizer.batch_decode(gen_tokens)[0]
-		post_dict = {}
-		post_dict["plain_text_caption"] = plain_text_caption
-		post_dict["gen_z_caption"] = gen_z_caption
-		post_dict["generated_caption"] = gen_text
-		final_captions.append(post_dict)
+		gen_caption = re.findall(r"gen-z social media caption: (.*)\n?", gen_text)[-1].replace("\n", "")
+		eval_data["plain_text_caption"].append(plain_text_caption)
+		eval_data["true_gen_z_caption"].append(gen_z_caption)
+		eval_data["generated_caption"].append(gen_caption)
+	
+	df = pd.DataFrame.from_dict(eval_data)
+	df.to_csv("evaluation_data.csv", index=False)
 
-	final_file = open('evaluation_data.csv', 'w')
-	writer = csv.writer(final_file)
-	writer.writerow(['plain_text_caption', 'gen_z_caption', 'generated_caption'])
-	for dictionary in final_captions: 
-		writer.writerow(dictionary.values())
-
-
+if __name__ == '__main__':
+    main()
 
 
 
